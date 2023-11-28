b6eff0cd913d84dc48af2893c9566c9b05460992
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-11-28 15:32:41 -0300

    setup database

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const swagger = require("swagger-ui-express");
+const swaggerFile = require("./swagger-file.json");
 
 const app = express();
 
@@ -8,5 +10,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerFile));
 
 module.exports = { app };
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const { app } = require("./app");
+const sequelize = require("./database/database");
 const { config } = require("dotenv");
 
 config();
@@ -7,6 +8,8 @@ process.on("unhandledRejection", (reason, promise) => {
   console.log(
     `App exiting due to an unhandled promise: ${promise} and reason: ${reason}`
   );
+
+  throw reason;
 });
 
 process.on("uncaughtException", (error) => {
@@ -14,8 +17,33 @@ process.on("uncaughtException", (error) => {
   process.exit(1);
 });
 
-const PORT = process.env.PORT;
+(async function () {
+  try {
+    const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Server is running in: http://localhost:${PORT}`);
-});
+    app.listen(PORT, () => {
+      console.log(`Server is running in: http://localhost:${PORT}`);
+    });
+
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+
+    const signals = ["SIGTERM", "SIGINT", "SIGQUIT"];
+
+    for (const signal of signals) {
+      process.on(signal, async () => {
+        try {
+          await sequelize.close();
+          console.log(`App exited with success`);
+          process.exit(0);
+        } catch (error) {
+          console.log(`App exited with error: ${error}`);
+          process.exit(1);
+        }
+      });
+    }
+  } catch (error) {
+    console.log(`App exited with error: ${error}`);
+    process.exit(1);
+  }
+})();
