8100343ed31ff50837c38c60d589f6ab1b0fe2e8
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-12-08 14:41:11 -0300

    validation

--- src/controllers/createUserController.js
+++ src/controllers/createUserController.js
@@ -1,28 +1,40 @@
 const Users = require("../models/Users");
+const RequestError = require("../utils/errors/errorHandler");
+
+const typeUsers = {
+  common: "common",
+  store: "store",
+};
 
 module.exports = {
   createUser: async (req, res) => {
     const { name, lastName, cpf, cnpj, email, password, typeUser, balance } =
       req.body;
 
-    try {
-      const user = await Users.create({
-        name: name,
-        last_name: lastName,
-        cpf: cpf,
-        cnpj: cnpj,
-        email: email,
-        password: password,
-        user_type: typeUser,
-        balance: balance,
+    if (typeUser != typeUsers.common && typeUser != typeUsers.store) {
+      res.status(417).send({
+        message: "the type of user must be: common or store",
       });
+    } else {
+      try {
+        const user = await Users.create({
+          name: name,
+          last_name: lastName,
+          cpf: cpf,
+          cnpj: cnpj,
+          email: email,
+          password: password,
+          user_type: typeUser,
+          balance: balance,
+        });
 
-      res.status(200).send({
-        message: "Account create",
-        data: user,
-      });
-    } catch (error) {
-      console.log(error);
+        res.status(200).send({
+          message: "Account create",
+          data: user,
+        });
+      } catch (error) {
+        console.log(error);
+      }
     }
   },
 };
--- src/controllers/transactionUserController.js
+++ src/controllers/transactionUserController.js
@@ -3,6 +3,10 @@ const Transactions = require("../models/Transactions");
 const RequestError = require("../utils/errors/errorHandler");
 const axios = require("axios");
 
+const statusMockExtern = {
+  1: "Autorizado",
+};
+
 async function httpCall() {
   const response = await axios.get(
     "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc"
@@ -14,28 +18,33 @@ async function httpCall() {
 module.exports = {
   createTransaction: async (req, res) => {
     const { senderId, receiverId, value } = req.body;
+    const status = await httpCall();
+    const userSender = await Users.findByPk(senderId);
+    const userReceiver = await Users.findByPk(receiverId);
 
-    try {
-      const userTransaction = await Transactions.create({
-        sender_id: senderId,
-        receiver_id: receiverId,
-        value: value,
-      });
-      const userSender = await Users.findByPk(senderId);
-      const userReceiver = await Users.findByPk(receiverId);
+    if (status.message == statusMockExtern[1]) {
+      try {
+        const userTransaction = await Transactions.create({
+          sender_id: senderId,
+          receiver_id: receiverId,
+          value: value,
+        });
 
-      userSender.balance -= value;
-      userReceiver.balance += value;
+        userSender.balance -= value;
+        userReceiver.balance += value;
 
-      await userSender.save();
-      await userReceiver.save();
+        await userSender.save();
+        await userReceiver.save();
 
-      return res.status(200).send({
-        message: "Transaction success",
-        data: userTransaction,
-      });
-    } catch (error) {
-      throw new RequestError("Transaction Fail", 500, error);
+        return res.status(200).send({
+          message: "Transaction success",
+          data: userTransaction,
+        });
+      } catch (error) {
+        throw new RequestError("Transaction Fail", 500, error);
+      }
+    } else {
+      throw new Error("transaction rejected");
     }
   },
 };
