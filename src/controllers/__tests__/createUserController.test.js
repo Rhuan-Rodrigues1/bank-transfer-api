047c08548a3c867bab36da203623884e817f7c9c
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2024-02-27 13:51:22 -0300

    fix: test users

--- src/controllers/__tests__/createUserController.test.js
+++ src/controllers/__tests__/createUserController.test.js
@@ -2,37 +2,24 @@ const request = require("supertest");
 const Users = require("../../models/Users");
 const { app } = require("../../app");
 
-const dataUserCommon = {
-  name: "RhuanStore",
-  lastName: "Rodrigues",
-  cpf: "1231342353",
-  cnpj: "",
-  email: "asd2@asd",
-  password: "1233",
-  balance: 1000,
-  typeUser: "store",
-};
-
 describe("Test Contoller", () => {
-  beforeEach(async () => {
-    await Users.drop();
-  });
   it("should return ok when account created successfully", async () => {
-    const res = await request(app).post("/create").send(dataUserCommon);
-
-    expect(res.status).toBe(200);
-    expect(res.body.message).toBe({
-      message: "Account create",
-      data: {
-        name: "RhuanStore",
-        lastName: "Rodrigues",
-        cpf: "1231342353",
-        cnpj: "",
-        email: "asd2@asd",
-        password: "1233",
-        balance: 1000,
-        typeUser: "store",
-      },
+    beforeEach(async () => {
+      await Users.drop();
     });
+    const res = await request(app).post("/create").send({
+      name: "names2df",
+      lastName: "la2stNamesdf",
+      cpf: "122",
+      email: "eas23d",
+      password: "passwoasdrd",
+      typeUser: "common",
+      balance: 1000,
+    });
+
+    console.log(res);
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.message).toBe("Account create");
   });
 });
